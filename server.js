f338faacddf526eaec781566a8ec77c224106423
Justin Bell <planbmanjb@gmail.com>
2017-12-06 18:53:19 -0500

    change to recipe

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+// we'll use morgan to log the HTTP layer
+const morgan = require('morgan');
+// we'll use body-parser's json() method to
+// parse JSON data sent in requests to this app
+const bodyParser = require('body-parser');
+
+// we import the ShoppingList model, which we'll
+// interact with in our GET endpoint
+//const {
+//    ShoppingList
+//} = require('./models');
+
+const {
+    Recipes
+} = require('./models');
+
+const jsonParser = bodyParser.json();
+const app = express();
+
+// log the http layer
+app.use(morgan('common'));
+
+// we're going to add some items to ShoppingList
+// so there's some data to look at. Note that
+// normally you wouldn't do this. Usually your
+// server will simply expose the state of the
+// underlying database.
+Recipes.create('chocolate milk', ['cocoa', 'milk', 'sugar']);
+Recipes.create('Grill Cheese', ['cheese', 'bread']);
+Recipes.create('Rice and Beans', ['Rice', 'beans']);
+
+// when the root of this route is called with GET, return
+// all current ShoppingList items by calling `ShoppingList.get()`
+app.get('/recipes', (req, res) => {
+    res.json(Recipes.get());
+});
+
+app.listen(process.env.PORT || 8080, () => {
+    console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
+});
